21a2966e5dd05e154a928a56f271f424ade5de8a
JoaxWalks <59808782+JoaxWalks@users.noreply.github.com>
2020-08-14 11:03:11 -0700

    Add files via upload

---
+++ lol.java
@@ -0,0 +1,12 @@
+
+public class lol {
+		public lol(String[] args) {
+			System.out.println("Hello World");
+			System.out.println("Hey it's me Mario");
+			System.out.println("Big fan of burgers");
+			System.out.println("actually want to make some games tho");
+			System.out.println("Big fan of that one wipeout game");
+			System.out.println("Big fan of playing monster hunter actually");
+		}
+	}
+
